8b453763c6c2a88da844cdaeefabe483b8ce63b0
Keara <keara.s.harris@gmail.com>
2021-09-27 11:17:15 -0400

    Updated package.json file to include updated versions of npm and nodemon, and updated GoogleStrategy for deployment to heroku and to utilize heroku's proxy server

--- services/passport.js
+++ services/passport.js
@@ -27,7 +27,8 @@ passport.use(
     {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true
     }, 
 //This is the checking if a user profile exists and if not creating a user
     (accessToken, refreshToken, profile, done) => {
