9abda299760b8bca245fcb68eef055bb91a3c248
Keara <keara.s.harris@gmail.com>
2021-10-11 21:17:49 -0400

    Added Header, Landing, Dashboard, and SurveyNew Components. Created a separate Header.js file for header component. These are React components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,26 @@
 //The app will export a compenent so the uppercase A is needed
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+//BrowerserRouter: brains of react-router, tells react-er how to behave and changes the components
+
+import Header from './Header'
+const Dashboard = () => <h2> Dashboard </h2>
+const SurveyNew =() => <h2> SurveyNew </h2>
+const Landing =() => <h2> Landing </h2>
 
 const App = () => {
     return (
         <div>
-            Hi There!
+            <BrowserRouter>
+                <div>
+                  {/*  Header being placed above components means it will always be at the top of the page*/}
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                   
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
